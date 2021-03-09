9826cf16620ef8f70a3b297ec3da061e432f15fa
oybek1024 <mukhiddinov.oybek1024@gmail.com>
2021-03-09 12:55:32 +0500

    celbrity assigned added

--- src/pages/Category/List.js
+++ src/pages/Category/List.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import BreadCrumbTemplete from "../../components/breadcrumb/BreadCrumbTemplete";
-import {Button, Card, Table, Tag } from "antd";
-import {PlusOutlined} from "@ant-design/icons";
+import { Button, Card, Table, Tag, Modal, Select, message } from "antd";
+import {PlusOutlined, PullRequestOutlined} from "@ant-design/icons";
 import { useHistory } from 'react-router-dom'
 import {isLoadingOverlay} from "@/redux/actions";
 import axios_init from "@/utils/axios_init";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from 'react-redux'
+
 export default function Contact() {
     const dispatch = useDispatch()
+    const { Option } = Select
     const { t } = useTranslation()
     const history = useHistory()
     const [items, setItems] = React.useState([])
+    const [celebrity, setCelebrity] = React.useState([])
     const routes = [
         {
             name: 'category',
@@ -49,14 +52,14 @@ export default function Contact() {
                 if (text) return <Tag color="#108ee9">{t('active')}</Tag>
                 else return <Tag color="#f50">{ t('inactive')} }</Tag>
             }
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+                <Button onClick={ () => { openModal(text.id) }} type="primary" icon={ <PullRequestOutlined /> } />
+            )
         }
-        // {
-        //     title: 'Action',
-        //     key: 'action',
-        //     render: (text, record) => (
-        //         <a>Delete</a>
-        //     )
-        // }
     ]
 
     const getData = function () {
@@ -68,8 +71,18 @@ export default function Contact() {
             dispatch(isLoadingOverlay(false))
         })
     }
+    const getCelebrity = function () {
+        axios_init.get('/celebrity', {
+            page: 1,
+            limit: 1000
+        }).then(res => {
+            console.log(res)
+            setCelebrity(res.celebrities)
+        })
+    }
     React.useEffect(() => {
         getData()
+        getCelebrity()
     }, [])
 
 
@@ -78,15 +91,83 @@ export default function Contact() {
             <Button onClick={ () => {
                 history.push('/category/create')
             }} type="primary" icon={<PlusOutlined />}>
-                Create
+                { t('create') }
             </Button>
         )
     }
+    const [visible, setVisible] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
+    const [categoryId, setCategoryId] = React.useState(null)
+    const [selectedCelebritys, setSelectedCelebritys] = React.useState([])
+    const hideModal = () => {
+        setVisible(false)
+    }
+    const selectCelebrity = (e) => {
+        setSelectedCelebritys([...e])
+    }
+    const openModal = (id) => {
+        setCategoryId(id)
+        // setLoadButton(true)
+        axios_init.get(`/category-celebrities/${id}`).then(res => {
+            if (!res.celebrities) setSelectedCelebritys([])
+            else setSelectedCelebritys(res.celebrities.map(e => e.id))
+            setVisible(true)
+        })
+    }
+    const assignCelebrity = function () {
+        if (selectedCelebritys.length !== 0) {
+            setLoading(true)
+            axios_init.post(`/category/${categoryId}/add-celebrity`, {
+                celebrity_ids: selectedCelebritys
+            }).then(res => {
+                message.success('Celebrity assigned successfully')
+                hideModal()
+                setLoading(false)
+            })
+        } else {
+            hideModal()
+        }
+        console.log(selectedCelebritys)
+        console.log('submit')
+    }
+    const CelebrityAssign = function () {
+        return (
+            <Modal
+                title={ t('add.celebrity') }
+                visible={visible}
+                maskClosable={false}
+                onCancel={hideModal}
+                footer={[
+                    <Button key="back" onClick={ () => { hideModal() } }>
+                        { t('cancel') }
+                    </Button>,
+                    <Button key="submit" type="primary" loading={loading} onClick={ () => { assignCelebrity() } }>
+                        { t('save') }
+                    </Button>
+                ]}
+            >
+                <Select
+                    mode="multiple"
+                    allowClear
+                    style={{ width: '100%' }}
+                    placeholder="Please select"
+                    onChange={ selectCelebrity }
+                    value={selectedCelebritys}
+                >
+                    {
+                        [...celebrity].map(e => <Option key={e.id} value={e.id}>{ e.first_name + ' ' + e.last_name }</Option> )
+                    }
+                </Select>
+            </Modal>
+        )
+    }
+
 
     return (
         <div>
             <BreadCrumbTemplete routes={routes}/>
             <Card title={t('celebrity')} extra={<ExtraButton/>}>
+                <CelebrityAssign />
                 <Table columns={columns} dataSource={items} rowKey={ (record) => record.id } scroll={{ x: 100 }}/>
             </Card>
         </div>
--- src/pages/Featured/List.js
+++ src/pages/Featured/List.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import BreadCrumbTemplete from "../../components/breadcrumb/BreadCrumbTemplete";
-import {Button, Card, Table, Tag } from "antd";
-import {PlusOutlined} from "@ant-design/icons";
+import {Button, Card, Table, Tag, Modal, Select, message } from "antd";
+import {PlusOutlined, PullRequestOutlined} from "@ant-design/icons";
 import { useHistory } from 'react-router-dom'
 import {isLoadingOverlay} from "@/redux/actions";
 import axios_init from "@/utils/axios_init";
@@ -9,9 +9,11 @@ import { useTranslation } from "react-i18next";
 import { useDispatch } from 'react-redux'
 export default function Contact() {
     const dispatch = useDispatch()
+    const { Option } = Select
     const { t } = useTranslation()
     const history = useHistory()
     const [items, setItems] = React.useState([])
+    const [celebrity, setCelebrity] = React.useState([])
     const routes = [
         {
             name: 'featured',
@@ -48,15 +50,15 @@ export default function Contact() {
             render: text => {
                 if (text) return <Tag color="#108ee9">{t('active')}</Tag>
                 else return <Tag color="#f50">{ t('inactive')} }</Tag>
-            },
+            }
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+                <Button onClick={ () => { openModal(text.id) }} type="primary" icon={ <PullRequestOutlined /> } />
+            )
         }
-        // {
-        //     title: 'Action',
-        //     key: 'action',
-        //     render: (text, record) => (
-        //         <a>Delete</a>
-        //     )
-        // }
     ]
 
     const getData = function () {
@@ -68,8 +70,18 @@ export default function Contact() {
             dispatch(isLoadingOverlay(false))
         })
     }
+    const getCelebrity = function () {
+        axios_init.get('/celebrity', {
+            page: 1,
+            limit: 1000
+        }).then(res => {
+            console.log(res)
+            setCelebrity(res.celebrities)
+        })
+    }
     React.useEffect(() => {
         getData()
+        getCelebrity()
     }, [])
 
 
@@ -82,11 +94,79 @@ export default function Contact() {
             </Button>
         )
     }
+    const [visible, setVisible] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
+    const [featuredId, setFeaturedId] = React.useState(null)
+    const [selectedCelebritys, setSelectedCelebritys] = React.useState([])
+    const hideModal = () => {
+        setVisible(false)
+    }
+    const selectCelebrity = (e) => {
+        setSelectedCelebritys([...e])
+    }
+    const openModal = (id) => {
+        setFeaturedId(id)
+        // setLoadButton(true)
+        axios_init.get(`/featured-celebrities/${id}`).then(res => {
+            if (!res.celebrities) setSelectedCelebritys([])
+            else setSelectedCelebritys(res.celebrities.map(e => e.id))
+            setVisible(true)
+        })
+    }
+    const assignCelebrity = function () {
+        if (selectedCelebritys.length !== 0) {
+            setLoading(true)
+            axios_init.post(`/featured/${featuredId}/add-celebrity`, {
+                celebrity_ids: selectedCelebritys
+            }).then(res => {
+                message.success('Celebrity assigned successfully')
+                hideModal()
+                setLoading(false)
+            })
+        } else {
+            hideModal()
+        }
+        console.log(selectedCelebritys)
+        console.log('submit')
+    }
+    const CelebrityAssign = function () {
+        return (
+            <Modal
+                title={ t('add.celebrity') }
+                visible={visible}
+                maskClosable={false}
+                onCancel={hideModal}
+                footer={[
+                    <Button key="back" onClick={ () => { hideModal() } }>
+                        { t('cancel') }
+                    </Button>,
+                    <Button key="submit" type="primary" loading={loading} onClick={ () => { assignCelebrity() } }>
+                        { t('save') }
+                    </Button>
+                ]}
+            >
+                <Select
+                    mode="multiple"
+                    allowClear
+                    style={{ width: '100%' }}
+                    placeholder="Please select"
+                    onChange={ selectCelebrity }
+                    value={selectedCelebritys}
+                >
+                    {
+                        [...celebrity].map(e => <Option key={e.id} value={e.id}>{ e.first_name + ' ' + e.last_name }</Option> )
+                    }
+                </Select>
+            </Modal>
+        )
+    }
+
 
     return (
         <div>
             <BreadCrumbTemplete routes={routes}/>
             <Card title={t('featured')} extra={<ExtraButton/>}>
+                <CelebrityAssign />
                 <Table columns={columns} dataSource={items} rowKey={ (record) => record.id } scroll={{ x: 100 }}/>
             </Card>
         </div>
